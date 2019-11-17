2e5cf3f272cff9e0fc543c6ac46ff0eff6a75e86
Vieja <mwiechcz@gmail.com>
2019-11-18 00:27:28 +0100

    add clickOn methods

--- ChessClient/src/logic/Controller.java
+++ ChessClient/src/logic/Controller.java
@@ -140,8 +140,13 @@ public class Controller implements Initializable {
         h7.setImage(new Image("file:images/pion_"+przeciwnik+".png"));
     }
 
-    public void clickA1(MouseEvent mouseEvent) {
-        int wyn = Game.onClick("A1");
+    public void  moveTower() {
+        a1.setImage(null);
+        a2.setImage(new Image("file:images/wieza_bialy.png"));
+    }
+
+    public void clickOn(String what) {
+        int wyn = Game.onClick(what);
         switch (wyn) {
             case 1 : {
                 moveTower();
@@ -150,8 +155,203 @@ public class Controller implements Initializable {
         }
     }
 
-    public void  moveTower() {
-        a1.setImage(null);
-        a2.setImage(new Image("file:images/wieza_bialy.png"));
+    public void clickA1(MouseEvent mouseEvent) {
+        clickOn("A1");
+    }
+    public void clickB1(MouseEvent mouseEvent) {
+        clickOn("B1");
+    }
+    public void clickC1(MouseEvent mouseEvent) {
+        clickOn("C1");
+    }
+    public void clickD1(MouseEvent mouseEvent) {
+        clickOn("D1");
+    }
+    public void clickE1(MouseEvent mouseEvent) {
+        clickOn("E1");
+    }
+    public void clickF1(MouseEvent mouseEvent) {
+        clickOn("F1");
+    }
+    public void clickG1(MouseEvent mouseEvent) {
+        clickOn("G1");
+    }
+    public void clickH1(MouseEvent mouseEvent) {
+        clickOn("H1");
+    }
+
+    public void clickA2(MouseEvent mouseEvent) {
+        clickOn("A2");
+    }
+    public void clickB2(MouseEvent mouseEvent) {
+        clickOn("B2");
+    }
+    public void clickC2(MouseEvent mouseEvent) {
+        clickOn("C2");
+    }
+    public void clickD2(MouseEvent mouseEvent) {
+        clickOn("D2");
+    }
+    public void clickE2(MouseEvent mouseEvent) {
+        clickOn("E2");
+    }
+    public void clickF2(MouseEvent mouseEvent) {
+        clickOn("F2");
+    }
+    public void clickG2(MouseEvent mouseEvent) {
+        clickOn("G2");
+    }
+    public void clickH2(MouseEvent mouseEvent) {
+        clickOn("H2");
+    }
+
+    public void clickA3(MouseEvent mouseEvent) {
+        clickOn("A3");
+    }
+    public void clickB3(MouseEvent mouseEvent) {
+        clickOn("B3");
+    }
+    public void clickC3(MouseEvent mouseEvent) {
+        clickOn("C3");
+    }
+    public void clickD3(MouseEvent mouseEvent) {
+        clickOn("D3");
+    }
+    public void clickE3(MouseEvent mouseEvent) {
+        clickOn("E3");
+    }
+    public void clickF3(MouseEvent mouseEvent) {
+        clickOn("F3");
+    }
+    public void clickG3(MouseEvent mouseEvent) {
+        clickOn("G3");
+    }
+    public void clickH3(MouseEvent mouseEvent) {
+        clickOn("H3");
+    }
+
+    public void clickA4(MouseEvent mouseEvent) {
+        clickOn("A4");
+    }
+    public void clickB4(MouseEvent mouseEvent) {
+        clickOn("B4");
+    }
+    public void clickC4(MouseEvent mouseEvent) {
+        clickOn("C4");
+    }
+    public void clickD4(MouseEvent mouseEvent) {
+        clickOn("D4");
+    }
+    public void clickE4(MouseEvent mouseEvent) {
+        clickOn("E4");
+    }
+    public void clickF4(MouseEvent mouseEvent) {
+        clickOn("F4");
+    }
+    public void clickG4(MouseEvent mouseEvent) {
+        clickOn("G4");
+    }
+    public void clickH4(MouseEvent mouseEvent) {
+        clickOn("H4");
+    }
+
+    public void clickA5(MouseEvent mouseEvent) {
+        clickOn("A5");
+    }
+    public void clickB5(MouseEvent mouseEvent) {
+        clickOn("B5");
+    }
+    public void clickC5(MouseEvent mouseEvent) {
+        clickOn("C5");
+    }
+    public void clickD5(MouseEvent mouseEvent) {
+        clickOn("D5");
+    }
+    public void clickE5(MouseEvent mouseEvent) {
+        clickOn("E5");
+    }
+    public void clickF5(MouseEvent mouseEvent) {
+        clickOn("F5");
+    }
+    public void clickG5(MouseEvent mouseEvent) {
+        clickOn("G5");
+    }
+    public void clickH5(MouseEvent mouseEvent) {
+        clickOn("H5");
+    }
+
+    public void clickA6(MouseEvent mouseEvent) {
+        clickOn("A6");
+    }
+    public void clickB6(MouseEvent mouseEvent) {
+        clickOn("B6");
+    }
+    public void clickC6(MouseEvent mouseEvent) {
+        clickOn("C6");
+    }
+    public void clickD6(MouseEvent mouseEvent) {
+        clickOn("D6");
+    }
+    public void clickE6(MouseEvent mouseEvent) {
+        clickOn("E6");
+    }
+    public void clickF6(MouseEvent mouseEvent) {
+        clickOn("F6");
+    }
+    public void clickG6(MouseEvent mouseEvent) {
+        clickOn("G6");
+    }
+    public void clickH6(MouseEvent mouseEvent) {
+        clickOn("H6");
+    }
+
+    public void clickA7(MouseEvent mouseEvent) {
+        clickOn("A7");
+    }
+    public void clickB7(MouseEvent mouseEvent) {
+        clickOn("B7");
+    }
+    public void clickC7(MouseEvent mouseEvent) {
+        clickOn("C7");
+    }
+    public void clickD7(MouseEvent mouseEvent) {
+        clickOn("D7");
+    }
+    public void clickE7(MouseEvent mouseEvent) {
+        clickOn("E7");
+    }
+    public void clickF7(MouseEvent mouseEvent) {
+        clickOn("F7");
+    }
+    public void clickG7(MouseEvent mouseEvent) {
+        clickOn("G7");
+    }
+    public void clickH7(MouseEvent mouseEvent) {
+        clickOn("H7");
+    }
+
+    public void clickA8(MouseEvent mouseEvent) {
+        clickOn("A8");
+    }
+    public void clickB8(MouseEvent mouseEvent) {
+        clickOn("B8");
+    }
+    public void clickC8(MouseEvent mouseEvent) {
+        clickOn("C8");
+    }
+    public void clickD8(MouseEvent mouseEvent) {
+        clickOn("D8");
+    }
+    public void clickE8(MouseEvent mouseEvent) {
+        clickOn("E8");
+    }
+    public void clickF8(MouseEvent mouseEvent) {
+        clickOn("F8");
+    }
+    public void clickG8(MouseEvent mouseEvent) {
+        clickOn("G8");
+    }
+    public void clickH8(MouseEvent mouseEvent) {
+        clickOn("H8");
     }
 }
